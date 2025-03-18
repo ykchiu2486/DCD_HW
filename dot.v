11c3b328b9d3fc19b72e7f88bd7b5c060ae753c7
ykc2486 <roger2486.cs13@nycu.edu.tw>
2025-03-18 12:59:12 +0800

    .

--- dot.v
+++ dot.v
@@ -1,9 +1,5 @@
-module dot(
-    input  [7:0] vec1, 
-    input  [7:0] vec2, 
-    output [8:0] result
-);
-    wire [7:0] mult0 = vec1[7:4] * vec2[7:4]; 
-    wire [7:0] mult1 = vec1[3:0] * vec2[3:0]; 
-    assign result = mult0 + mult1;
+module dot(input [7:0] vec1, input [7:0] vec2, output [8:0] result);
+    wire [7:0] m0 = vec1[7:4] * vec2[7:4]; 
+    wire [7:0] m1 = vec1[3:0] * vec2[3:0]; 
+    assign result = m0 + m1;
 endmodule
--- matrix_top.v
+++ matrix_top.v
@@ -1,8 +1,4 @@
-module matrix_top(
-    input  [15:0] A, 
-    input  [7:0]  x, 
-    output [17:0] y  
-);
+module matrix_top(input [15:0] A, input [7:0]  x, output [17:0] y);
     wire [8:0] y0, y1;
     
     dot dot0 (.vec1(A[15:8]), .vec2(x), .result(y0)); 
